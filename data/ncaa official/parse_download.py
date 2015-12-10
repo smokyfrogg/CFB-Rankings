2d045c6ac114445c912b7ecfe7b258c826122250
smokyfrogg <rtmichaels@gmail.com>
2015-12-10 14:11:44 -0500

    parse_download - finished check_args

--- data/ncaa official/parse_download.py
+++ data/ncaa official/parse_download.py
@@ -7,22 +7,33 @@
 #		directory ./filename/ filled with individual stats files as 'filename''stattype'.txt
 #		'stattype' is the section header from the stats files
 
-import sys
+import sys, os
 
 #check for args
-
+if (!check_args()):
+	return
 #check for correct format
+
+#prep output folder if necessary
 
 #iterate through each chunk, writing to new file
 
 #close out input
 
 #success/fail notice
+return
 
-def check_args():
-	return
-
+def check_args(): 	#Checks the command line arguments, returns True if valid, False otherwise
+					#Prints reason for failure if False
+	if (len(sys.argv) != 2):
+		print 'Incorrect number of arguments. Provide only the filename of data to be split as an argument.'
+		return False
+	if (os.path.isfile(argv[1])):
+		return True
+	return False
 def check_format():
+	return
+def clean_output():
 	return
 def write_chunk():
 	return
