9391a8ef3790c588c835573911bf981648998f8c
smokyfrogg <rtmichaels@gmail.com>
2015-12-10 13:53:13 -0500

    started parser

---
+++ data/ncaa official/parse_download.py
@@ -0,0 +1,20 @@
+#Usage 
+#Input: 
+#		provide command line argument of 'filename', the ASCII formatted stats from 
+#		the official NCAA stats page (http://web1.ncaa.org/stats/StatsSrv/rankings?sportCode=MFB&academicYear=2016)
+#
+#Output:
+#		directory ./filename/ filled with individual stats files as 'filename''stattype'.txt
+#		'stattype' is the section header from the stats files
+
+import sys
+
+#check for args
+
+#check for correct format
+
+#iterate through each chunk, writing to new file
+
+#close out input
+
+#success/fail notice
