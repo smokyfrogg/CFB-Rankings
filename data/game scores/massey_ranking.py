1d52cf4577375d8f649cfa48a61d90fab270cd2f
smokyfrogg <rtmichaels@gmail.com>
2015-12-29 12:06:01 -0500

    some cleanup from old blank files

---
+++ data/game scores/massey_ranking.py
@@ -0,0 +1,8 @@
+#Usage 
+#Input: 
+#		provide command line argument of 'filename', the ASCII formatted stats from 
+#		the Ken Massey's stats page (http://www.masseyratings.com/data.php)
+#
+#Output:
+#		txt file with list of teams sorted by Massey's LSR ranking method based on point differential
+#		http://www.masseyratings.com/theory/massey97.pdf
